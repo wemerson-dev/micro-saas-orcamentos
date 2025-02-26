a8b564155fb05391cea18f1c8b46834cd7e3ff59
Wemerson Renan <101459889+wemerson-dev@users.noreply.github.com>
2025-02-25 21:01:36 -0300

    Front do zero

--- frontend/src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-      <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-2xl">
-        Tailwind CSS está funcionando! 🚀
-      </div>
-    );
-  }
