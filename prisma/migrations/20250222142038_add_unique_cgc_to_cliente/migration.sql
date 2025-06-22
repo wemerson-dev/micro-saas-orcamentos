b46409cf91337c4560e7016b0fd1e2ecf275a8b5
wemerson-dev <wemerson_renan@hotmail.com>
2025-06-22 11:31:47 -0300

    Supabase

--- prisma/migrations/20250222142038_add_unique_cgc_to_cliente/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[cgc]` on the table `Cliente` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Cliente_cgc_key" ON "Cliente"("cgc");
--- prisma/migrations/20250421194523_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Cliente" ALTER COLUMN "cgc" SET DATA TYPE TEXT;
