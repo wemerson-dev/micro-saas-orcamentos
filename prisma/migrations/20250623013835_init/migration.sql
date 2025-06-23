983d51e67454eefa77e04ac3dd04b908c70158d7
wemerson-dev <wemerson_renan@hotmail.com>
2025-06-22 22:40:18 -0300

    Bancode Dados

---
+++ prisma/migrations/20250623013835_init/migration.sql
@@ -0,0 +1,68 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cliente" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "bairro" TEXT NOT NULL,
+    "numero" INTEGER NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT,
+    "cgc" TEXT NOT NULL,
+    "usuarioId" TEXT NOT NULL,
+
+    CONSTRAINT "Cliente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Orcamento" (
+    "id" TEXT NOT NULL,
+    "numOrc" INTEGER NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pendente',
+    "dataEmissao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "clienteId" TEXT NOT NULL,
+
+    CONSTRAINT "Orcamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ItemOrcamento" (
+    "id" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "precoUnitario" DOUBLE PRECISION NOT NULL,
+    "orcamentoId" TEXT NOT NULL,
+
+    CONSTRAINT "ItemOrcamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cliente_email_key" ON "Cliente"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cliente_cgc_key" ON "Cliente"("cgc");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Orcamento_numOrc_key" ON "Orcamento"("numOrc");
+
+-- AddForeignKey
+ALTER TABLE "Cliente" ADD CONSTRAINT "Cliente_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Orcamento" ADD CONSTRAINT "Orcamento_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "Cliente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ItemOrcamento" ADD CONSTRAINT "ItemOrcamento_orcamentoId_fkey" FOREIGN KEY ("orcamentoId") REFERENCES "Orcamento"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
